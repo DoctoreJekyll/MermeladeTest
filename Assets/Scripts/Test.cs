8e27b50d7390f1425893fce8a8973c2ae356e51f
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-11-04 19:54:46 +0100

    changes

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+
+    private Rigidbody2D rb2d;
+    [SerializeField] private float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var inputsHorizontal = Input.GetAxisRaw("Horizontal");
+        var inputsVertical = Input.GetAxisRaw("Vertical");
+
+        var inputs = new Vector2(inputsHorizontal, inputsVertical).normalized;
+
+        rb2d.velocity = inputs * speed;
+
     }
 }

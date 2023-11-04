d8cd6486bed6be21ccdb7b968a3bfad087f2b6a4
KoriChanBear <149896746+KoriChanBear@users.noreply.github.com>
2023-11-04 21:01:57 +0100

    Commit Ana Test

---
+++ Assets/Scripts/AnaTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnaTest : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2;
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+    private void movement()
+    {
+        var input = Input.GetAxisRaw("Horizontal");
+        var input2 = Input.GetAxisRaw("Vertical");
+        rb2.velocity = new Vector2(input * speed, input2 * speed);
+    }
+}

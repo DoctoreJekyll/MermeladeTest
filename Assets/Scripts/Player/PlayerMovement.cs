703985e62b11b9cde1415137c6cb2b0071e6284e
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-11-17 19:41:36 +0100

    Feat: Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        private Rigidbody2D rb2d;
+        private Vector2 movementDirections;
+
+        [SerializeField] private float speed;
+        [SerializeField] private float runAccelAmount;
+        [SerializeField] private float runDeccelAmount;
+        
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            PlayerInputsValue();
+        }
+
+        private void FixedUpdate()
+        {
+            PlayerMoveImprove();
+        }
+
+        private void PlayerInputsValue()
+        {
+            movementDirections.x = Input.GetAxisRaw("Horizontal");
+            movementDirections.y = Input.GetAxisRaw("Vertical");
+        }
+        
+        private void PlayerMoveImprove()
+        {
+            Vector2 rate = new Vector2(SpeedDelta().x * AccelerationRate().x, SpeedDelta().y * AccelerationRate().y);
+            rb2d.AddForce(rate, ForceMode2D.Force);
+        }
+
+        private Vector2 TargetSpeed()
+        {
+            Vector2 targetSpeed = new Vector2(movementDirections.x, movementDirections.y).normalized * speed;
+            
+            return targetSpeed;
+        }
+
+        private Vector2 AccelerationRate()
+        {
+            Vector2 accelRate = new Vector2((Mathf.Abs(TargetSpeed().x) > 0.01f) ? runAccelAmount : runDeccelAmount,
+                (Mathf.Abs(TargetSpeed().y) > 0.01f) ? runAccelAmount : runDeccelAmount);
+            
+            return accelRate;
+        }
+
+        private Vector2 SpeedDelta()
+        {
+            Vector2 sDelta = new Vector2(TargetSpeed().x - rb2d.velocity.x, TargetSpeed().y - rb2d.velocity.y);
+            return sDelta;
+        }
+    }
+}

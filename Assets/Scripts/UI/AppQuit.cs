7451a38494777fafc1ab79c14a864b6c2bf2c952
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2023-11-19 14:52:48 +0100

    nuevos props

---
+++ Assets/Scripts/UI/AppQuit.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class AppQuit : MonoBehaviour
+    {
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    
+    
+    }
+}
